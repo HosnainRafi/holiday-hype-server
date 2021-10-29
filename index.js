c4935ed824e3289de9b26f15887d9e3a221bf6c0
HosnainRafi <rhosnain@gmail.com>
2021-10-29 19:57:42 +0600

    Almost Done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors')//cors for own server connected with own
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 require("dotenv").config();//dotenv config
 const port = process.env.PORT || 5000;
@@ -17,30 +18,61 @@ console.log(uri);
 
 async function run() {
     try {
-      await client.connect();
-      const database = client.db("Travel-Hype");
-      const servicesCollection = database.collection("services");
-      const usersCollection = database.collection("users");
-      
-      //AddServices
-      app.post('/addServices', async (req,res) => {
-        const newService = req.body;
-        const result = await servicesCollection.insertOne(newService);
-        res.json(result);
-      })
+        await client.connect();
+        const database = client.db("Travel-Hype");
+        const servicesCollection = database.collection("services");
+        const eventsCollection = database.collection("users");
+
+        //getServices
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            console.log(services);
+            res.send(services);
+        })
+
+        //AddServices
+        app.post('/addServices', async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.json(result);
+        })
+
+        //Find single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        // add events
+        app.post("/addEvent", async (req, res) => {
+            console.log(req.body);
+            const result = await eventsCollection.insertOne(req.body);
+            console.log(result);
+        });
+
+        //My events
+        app.get("/myEvents/:email", async (req, res) => {
+            const result = await eventsCollection.find({
+              email: req.params.email,
+            }).toArray();
+            res.send(result);
+          });
 
 
     } finally {
-    //   await client.close();
+        //   await client.close();
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send('Server is running')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Port is running');
 })
